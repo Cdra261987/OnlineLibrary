005b7569df8ffd694ff9ae6f18be6fa6ace0e48e
Cdra261987 <anaritacatarino.87@gmail.com>
2024-06-03 17:32:18 +0100

    OnlineLibrary.ConsoleTest

---
+++ OnlineLibrary.ConsoleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using OnlineLibrary.Data.Context;
+using OnlineLibrary.Domain.Entities;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace OnlineLibrary.ConsoleTest
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		      	Book book = new Book()
+			      {
+				        Title = "Aprender.NET",
+				        Description = "Livro para aprender.NET",
+				        Author = "Jose Manuel",
+				        Year = 2019,
+			        	Category = "Tecnologia"
+			      };
+      
+      OnlineLibraryDBContext onlineLibraryDBContext = new OnlineLibraryDBContext();
+      
+		}
+	}
+}
