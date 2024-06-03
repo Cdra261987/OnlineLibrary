b75cbada8cd5d94ac4b950bf0401b3d7ccba0120
Cdra261987 <anaritacatarino.87@gmail.com>
2024-06-03 17:37:11 +0100

    OnlineLibrary.Domain

---
+++ OnlineLibrary.Domain/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineLibrary.Domain.Entities
+{
+	public class Book
+	{
+  public int Id { get; set; }
+  public string Title { get; set; }
+  public string Description { get; set; }
+  public int Year { get; set; }
+  public string Category { get; set; }
+  
+
+	}
+}
